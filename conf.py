5038e19bb83ac0a3073d632808afe3b1b17f9a0e
sucharitha08 <74643077+sucharitha08@users.noreply.github.com>
2020-11-21 09:41:13 +0530

    Updated author names

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Handwriting Detection using Deep learning'
+copyright = '2020, Sucharitha Sanchi'
+author = 'Sucharitha Sanchi'
 
 # The full version, including alpha/beta/rc tags
 release = ''
